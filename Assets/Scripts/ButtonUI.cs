91f07af82bda05967c839b230c2a94fc680894c3
EthanCrisford <476052@chrome.escg.ac.uk>
2022-12-12 10:59:22 +0000

    1

--- Assets/Scripts/ButtonUI.cs
+++ Assets/Scripts/ButtonUI.cs
@@ -3,25 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using JetBrains.Annotations;
-using UnityEngine.UIElements;
 
-public class ButtonUI : MonoBehaviour
+public class ButtonScript : MonoBehaviour
 {
-    public bool Button = false;
-
+    Color col;
+    Image img;
 
     // Start is called before the first frame update
     void Start()
     {
-        Button = GetComponent<Button>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if Input.GetButtonDown = true;
-        {
-            Button.color = new(Button.color.r = 0, Button.color.g = 0, Button.color.b = 0);
-        }
+        col = GetComponent<Image>().color;
+        col.a = 0;  // image alpha = 0 - image off
+        col.a = 31;  // image alpha = 31 - image on
+        GetComponent<Image>().color = col;
     }
 }
